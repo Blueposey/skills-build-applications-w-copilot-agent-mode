236ca551fe7d23332b11a51ce78f984d315a82a6
stacey <staceyno14@gmail.com>
2025-04-08 02:28:07 +0000

    Commit changes

---
+++ octofit-tracker/frontend/src/components/Users.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://verbose-pancake-r4q9j4jwqpp257q6-8000.app.github.dev/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-center my-4">Users</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Users;
